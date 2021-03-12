ef022505bd15f4e0ce43ea54d62c0ecd4d17d81f
JessicaAquino <jessica.aquino.torrez@gmail.com>
2021-03-12 11:16:47 -0400

    Add project files.

---
+++ ExerciseFlow/ExerciseFlow/ApiProductor/Controllers/OdometerController.cs
@@ -0,0 +1,36 @@
+﻿namespace ApiProductor.Controllers
+{
+    using ApiProductor.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using System;
+    using System.Threading.Tasks;
+
+    using Azure.Messaging.ServiceBus;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OdometerController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Odometer odometer)
+        {
+            string connectionString = "Endpoint=sb://queuejess.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=sNf2UfOxLet90gXq03ChFxRaGL/cqy4avqGAVfKVSCk=;EntityPath=queueejercicios";
+            string queueName = "queueejercicios";
+            string mensaje = JsonConvert.SerializeObject(odometer);
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return true;
+        }
+    }
+}
---
+++ ExerciseFlow/ExerciseFlow/ApiProductor/Models/Odometer.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiProductor.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Odometer
+    {
+        [Key]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime Datetime { get; set; }
+        [Required]
+        public long Steps { get; set; }
+    }
+}
+    
