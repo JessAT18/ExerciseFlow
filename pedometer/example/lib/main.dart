fe4f6bf73512cc61e53da40666829c7d2be662e3
JessicaAquino <jessica.aquino.torrez@gmail.com>
2021-03-15 15:18:31 -0400

    Adding Dart Application Project

---
+++ pedometer/example/lib/main.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:pedometer/pedometer.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  //Controllers for textbox
+  final emailController = TextEditingController();
+  final nameController = TextEditingController();
+
+  Stream<StepCount> _stepCountStream;
+  Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '?';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  Future<String> sendData() async{ //New Function to send emails
+    var now = DateTime.now();
+    String cadena = "{"+
+    "'Email':'"+ emailController.text +"',"+ //textfield value
+    "'Name':'"+ nameController.text +"',"+   //textfield value
+    "'Datetime':'"+ now.toIso8601String() +"',"+ //variable for datetime
+    "'Steps':'"+ _steps +"'"+ //variable for steps
+    "}";
+    var data = jsonEncode(cadena);
+    var response = await http.post(
+      Uri.https('apiproductorjess.azurewebsites.net', '/api/odometer'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: data
+    );
+    return response.body;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Odometer'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Steps taken:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                _steps,
+                style: TextStyle(fontSize: 60),
+              ),
+              Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              Text(
+                'Pedestrian status:',
+                style: TextStyle(fontSize: 30),
+              ),
+              Icon(
+                _status == 'walking'
+                    ? Icons.directions_walk
+                    : _status == 'stopped'
+                        ? Icons.accessibility_new
+                        : Icons.error,
+                size: 100,
+              ),
+              Center(
+                child: Text(
+                  _status,
+                  style: _status == 'walking' || _status == 'stopped'
+                      ? TextStyle(fontSize: 30)
+                      : TextStyle(fontSize: 20, color: Colors.red),
+                ),
+              ),
+              Divider(
+                height: 100,
+                thickness: 0,
+                color: Colors.white,
+              ),
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    new TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email'
+                      ),
+                    ),
+                    new TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Name'
+                      ),
+                    ),
+                    new ElevatedButton(
+                      onPressed: sendData,
+                      child: new Text("Enviar datos")
+                    ),
+                  ]
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
